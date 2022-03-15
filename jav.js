7ab7b70add77022a880ca7728324791d6b6840f1
Kuunapor Doreen Mwinkuma <kuunapordoreen@gmail.com>
2022-03-15 11:23:23 +0000

    Delete jav.js

--- jav.js
+++
@@ -1,4 +0,0 @@
-let firstName = prompt("Hi 👋 What's your name?");
-let hi = ("Hi")
-let welcome = ("welcome to my first ever website coded by me.")
-alert(hi + " " +  firstName + ", " + " " + welcome)
